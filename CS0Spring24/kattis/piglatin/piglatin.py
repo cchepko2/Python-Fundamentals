7609e05ed6488faaf4ba447a44e36754ccabcede
Corin Chepko <cchepko2@coloradomesa.edu>
2024-03-08 17:06:35 -0700

    g

--- CS0Spring24/kattis/piglatin/piglatin.py
+++ CS0Spring24/kattis/piglatin/piglatin.py
@@ -1,3 +1,5 @@
+import string
+
 '''
 Corin Chepko
 3/6/24
@@ -13,3 +15,32 @@ Algorithm Steps:
                     add 'yay' to end of word
             print piglatin line by joining words using join method
 '''
+
+def piglatin(word: str):
+    '''
+    Converts a word into piglatin and returns that string
+    '''
+    vowels = ['a','e','i','o','u']
+    if(word[0] in vowels):
+        word = word + 'yay'
+    else:       #it begins with a consonant
+        index = 0
+        for c in word:
+            if c in vowels: #found first vowel
+                beg = word[0:index]
+                word = word[index::] + beg + 'ay'
+                break
+            index+=1
+    return word
+
+while(True):
+    try:
+        phrase = input()
+    except EOFError:
+        break
+    words = input().split()
+    for word in words:
+        translation = piglatin(word)
+        print(translation, end=' ')
+    print()
+
