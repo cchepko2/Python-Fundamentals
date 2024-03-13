2a4ffded230f13319ee0c0862e16b62ce4ebbe5f
Corin Chepko <cchepko@gmail.com>
2024-03-13 11:32:33 -0600

    drg

---
+++ CS0Spring24/kattis/piglatin/piglatin2.py
@@ -0,0 +1,52 @@
+import string
+
+'''
+Corin Chepko
+3/6/24
+Kattis problem: Piglatin https://open.kattis.com/problems/piglatin
+Algorithm Steps:
+    While reading lines in until end of file:
+        separate the line in words
+        for each word:
+            convert to piglatin by:
+                if word begins with consonant:
+                    move all letters before vowel to end of word and add 'ay'
+                else:
+                    add 'yay' to end of word
+            print piglatin line by joining words using join method
+'''
+
+import sys
+
+def piglatin(word: str):
+    '''
+    Converts a word into piglatin and returns that string
+    '''
+    vowels = ['a','e','i','o','u','y']
+    if(word[0] in vowels):
+        word = word + 'yay'
+    else:       #it begins with a consonant
+        index = 0
+        for c in word:
+            if c in vowels: #found first vowel
+                beg = word[0:index]
+                word = word[index::] + beg + 'ay'
+                break
+            index+=1
+    return word
+
+for words in sys.stdin:
+    if not words:
+        break
+    #try:
+    #    phrase = input()
+    #except EOFError:
+    #    break
+    translated = ""
+    words = words.split()
+    for word in words:
+        translated = translated + piglatin(word) + ' '
+    
+    print(translated[0:len(translated)-1]) # ommit extra space at end
+    
+
