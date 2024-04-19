e230dfe3443436a675fe26d1bf5a689ad2ff2056
Corin Chepko <cchepko2@coloradomesa.edu>
2024-04-19 16:13:24 -0600

    PySkel_plot_pixel

--- CS0Spring24/pygame_skel.py
+++ CS0Spring24/pygame_skel.py
@@ -19,12 +19,13 @@ def gameloop():
                     run = False
 
         # Draw everything
-        main_surface.fill((30, 0, 30)) #fill background color
+        
         # Draw stuff
-        color = (0,255,0)
-        p1 = (0,0)
-        p2 = (surface_size,surface_size)
-        pygame.draw.line(main_surface, color, p1, p2)
+        color = (255,0,0)
+        p1 = (surface_size/2,surface_size/2)
+        main_surface.set_at(p1, color) #Plot a pixel at (mid_point[0],mid_point[1])
+        #p2 = (surface_size,surface_size)
+        #pygame.draw.line(main_surface, color, p1, p2)
 
         pygame.display.flip() # Put all the drawing up to the display
         my_clock.tick(120) #Keeps a contant framerate for smoother animation
