f422ac4f6b5a71ae8b8acf6c632788ad5a0d0800
Corin Chepko <cchepko2@coloradomesa.edu>
2024-09-30 16:50:58 -0600

    Sep30

---
+++ ClassNotesFall24/Sep30_strings/sorting.py
@@ -0,0 +1,13 @@
+list_nums = [1,2,3,4,10,20,101,200] # lists are in square brackets
+list_snums = ['1','2','3','4','10','20','101','200'] # list of strings
+
+print(list_nums)
+list_nums.sort()
+print(list_nums)
+
+print()
+
+# lists of strings sort alphabetically, not numerically
+print(list_snums)
+list_snums.sort()
+print(list_snums)
