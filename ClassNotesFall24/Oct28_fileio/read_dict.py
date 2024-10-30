0ee2f5ab8c11c6dfe800b7d129376c591fa788f6
Corin Chepko <cchepko2@coloradomesa.edu>
2024-10-30 17:07:06 -0600

    game

--- ClassNotesFall24/Oct28_fileio/read_dict.py
+++ ClassNotesFall24/Oct28_fileio/read_dict.py
@@ -1,26 +1,23 @@
 import json
 import os
 import random
-from urllib import request
+import pathlib
 
-def retrieve_page(url, file_name):
-    """ Download the contents of a web page.
-    """
-    response = request.urlretrieve(url, file_name)
+my_path = pathlib.Path(__file__).parent.resolve()
 
-file_name = 'EDMTDictionary.json'
-if os.path.isfile(file_name) != True:
-    retrieve_page('https://github.com/eddydn/DictionaryDatabase/raw/master/EDMTDictionary.json', file_name)
+file_name = 'words.dict'
+file_name = f'{my_path}/{file_name}'
+
+if os.path.exists(file_name):
+    with open(file_name) as json_dict:
+        words_dict = json.load(json_dict)
+        print(type(words_dict))
+        print('There are {0} words in the file.'.format(len(words_dict)))
+        print(words_dict[2584])
+        print(type(words_dict[2584]))
+        
+        choice = random.choice(words_dict)
+        print('{}\n{}\n{}'.format(choice['word'], \
+            choice['type'], choice['description']))
 else:
-    print("Already have the dictionary")
-
-with open("EDMTDictionary.json") as json_dict:
-    words_dict = json.load(json_dict)
-    print(type(words_dict))
-    print('There are {0} words in the file.'.format(len(words_dict)))
-    print(words_dict[2584])
-    print(type(words_dict[2584]))
-    
-    choice = random.choice(words_dict)
-    print('{}\n{}\n{}'.format(choice['word'], \
-        choice['type'], choice['description']))
+    print(f"Cannot open the file {file_name}.")
---
+++ ClassNotesFall24/Oct30_Game_stats_demo/game.py
@@ -0,0 +1,41 @@
+import quick_read_words as read_words
+import random
+
+def run_game(words):
+    word_of_day = random.choice(words)
+    return random.choice(["You win", "You lose", "Tied!"])
+
+
+def main():
+    print("Welcome to my game!")
+    user = input("Enter your username: ")
+    stats = list(read_words.get_user_stats(user))
+    print(f"User stats: {stats}")
+
+    while(True):
+        words = read_words.get_words("words.txt")
+        stat = run_game(words)
+        print(stat)
+
+        if stat == "You win":
+            stats[0] += 1 # increment game counter
+            stats[1] += 1 # increment wins
+        elif stat == "You lose":
+            stats[0] += 1 # increment game counter
+        else:
+            # tied
+            stats[0] += 1 # increment game counter
+            stats[2] += 1 # increment ties
+
+
+        print("Your latest stats: ")
+        print(stats)
+
+        again = input("Do you want to play again (y/n)?")
+        if( again.upper() != 'Y'):
+            break
+    # End while(game) loop
+    read_words.write_user_stats(user, stats)
+
+if __name__ == "__main__":
+    main()
