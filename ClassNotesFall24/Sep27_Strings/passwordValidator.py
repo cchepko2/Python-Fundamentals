b901325b3e7c3a4f40a90f06e7dfb463bbda8e97
sethkfman <sethkfman@gmail.com>
2024-11-13 20:25:43 -0700

    Revert "added algorithm steps"
    This reverts commit 2b361efc754245f5749c69e1fdad308adb150f80.

--- ClassNotesFall24/Sep27_Strings/passwordValidator.py
+++ ClassNotesFall24/Sep27_Strings/passwordValidator.py
@@ -2,7 +2,7 @@
 Password Validator
 Updated By: Seth Kaufman
 CSCI 110 Lecture
-Date: 27 SEP 2024
+Date: 25 SEP 2024
 
 Build a password validator that:
 - Takes in a string
@@ -14,10 +14,7 @@ Build a password validator that:
 
 Algorithm:
     1. init counters
-    2. create loop to look at each character in string  
-    3. check if each character is an upper, lower, punucation or number
-    4. check if there an error and print out message is the password does not match
-    5. print out error/success message
+    2. 
 """
 import string
 
@@ -41,6 +38,7 @@ def passwordValidator(password:str):
             number_count += 1 
     
     # check if there an error and print out message is the password does not match
+        # Check each condition
     error = ""
     if upper_count < 2:
         error += "Password must contain at least 2 uppercase letters. "
