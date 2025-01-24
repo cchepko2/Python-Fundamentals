6ae2eaac1fd1b0b527b977529bb5fad6691101f9
Corin Chepko <cchepko2@coloradomesa.edu>
2025-01-24 15:51:25 -0700

    NotesEndofClass

--- ClassNotesSpring25/01-24-25_Intro/FirstNotes.py
+++ ClassNotesSpring25/01-24-25_Intro/FirstNotes.py
@@ -1,1 +1,26 @@
 # First day of notes
+
+# Example Program Header
+#----------------------------------------------------------
+# hello world program
+# by: John Doe
+# Jan 1 2017
+# Copyright: Anyone may freely copy or modify this program
+#----------------------------------------------------------
+
+# Alternatively, triple quotes can be used for comments 
+'''
+Corin Chepko
+1/24/25
+Program: Hello World
+Algorithm: print("Hello World")
+'''
+
+
+print('Hello World!') # Say hello to the beautiful world!
+
+name = input("Enter your name: ") # When we collect input, we get a string
+print( type(name) )  # Tell what type of varible name is... a string
+
+print("Hello " + "World") # We can add strings together
+print("Hello " + name) # Since name is a string, we can add it to "Hello "
