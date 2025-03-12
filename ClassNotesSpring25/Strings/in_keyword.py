7c92eed05ddc6f53bf7ecd990058a261c020948a
Corin Chepko <cchepko2@coloradomesa.edu>
2025-03-12 15:50:53 -0600

    wetg

---
+++ ClassNotesSpring25/Strings/in_keyword.py
@@ -0,0 +1,12 @@
+phrase = "Someone says jump!"
+
+found = phrase.find("Simon says")
+print(found)
+
+found = phrase.find("Someone says")
+print(found)
+
+found = "Someone says" in phrase
+print(found)
+
+print('ys' in phrase)
