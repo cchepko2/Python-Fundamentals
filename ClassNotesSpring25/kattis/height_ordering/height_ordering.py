25d48fbc9ac7e46645bcb7e1a8c008325803e1a3
Corin Chepko <cchepko2@coloradomesa.edu>
2025-03-26 15:50:00 -0600

    lists

--- ClassNotesSpring25/kattis/height_ordering/height_ordering.py
+++ ClassNotesSpring25/kattis/height_ordering/height_ordering.py
@@ -9,15 +9,27 @@ Algorithm Steps:
             input test number and 20 numbers for heights
 '''
 
+def steps_back(heights):
+    total_steps_taken = 0
+
+    # outter loop goes through each height starting from index 1
+        #inner loop goes through all previous indexs to see if someone is shorter
+    
+    return total_steps_taken
+
+
 import sys
 
 print("Enter number of test cases: ", file=sys.stderr)
 number_tests = int(input())
 
+inputs = []
+
 for i in range(1, number_tests+1): # Starting from count of one, collect number_test sets of numbers
     line_nums = input().split() # Collect line of numbers and split into list
     line_nums = line_nums[1::]  # slice off first number, we don't need it
     line_nums = list(map(int, line_nums))
+    inputs.append(line_nums)
     # Handy function to convert list to number of steps taken back goes here
 
-print(line_nums)
+print(inputs)
