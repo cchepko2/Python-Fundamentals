178d74186118cac4c094137c7b0d92d1bf3dbfdb
Corin Chepko <cchepko2@coloradomesa.edu>
2025-09-02 16:52:09 -0600

    sf

---
+++ Classnotes_Fall25/9-1-25_bitwise/1.py
@@ -0,0 +1,12 @@
+'''
+Binaray and hex numbers
+'''
+
+number = 0b1010
+
+print(f"{number=}")
+
+number = 0b11111111111111111111111111111111
+True
+
+print(f"{number=}")
--- Classnotes_Fall25/Hello/hello.py
+++ Classnotes_Fall25/Hello/hello.py
@@ -4,6 +4,10 @@ Date: 8/22/25
 Program: Kattis problem - https://open.kattis.com/problems/hello
 '''
 
+<<<<<<< Updated upstream
 print("Hello World!")
 
 print("more")
+=======
+print("Hello World♡!")
+>>>>>>> Stashed changes
