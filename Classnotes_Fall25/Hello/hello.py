a15491ad67178bbe0b9a6a85d463bced95719ea8
Corin Chepko <cchepko2@coloradomesa.edu>
2025-09-02 16:52:52 -0600

    sf

--- Classnotes_Fall25/Hello/hello.py
+++ Classnotes_Fall25/Hello/hello.py
@@ -4,10 +4,4 @@ Date: 8/22/25
 Program: Kattis problem - https://open.kattis.com/problems/hello
 '''
 
-<<<<<<< Updated upstream
-print("Hello World!")
-
-print("more")
-=======
 print("Hello World♡!")
->>>>>>> Stashed changes
