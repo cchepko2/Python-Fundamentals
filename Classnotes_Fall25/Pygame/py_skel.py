cbeb5ccc06f200f303edb563f1673bdae2762b73
Corin Chepko <cchepko2@coloradomesa.edu>
2025-11-14 16:14:32 -0700

    Fractal

--- Classnotes_Fall25/Pygame/py_skel.py
+++ Classnotes_Fall25/Pygame/py_skel.py
@@ -1,9 +1,10 @@
 import pygame, math, random
+
 pygame.init()           # prepare the pygame module for use
 
 # Create a new surface and window.
-surface_size_x = 1000   # Pixels X
-surface_size_y = 700    # Pixels Y
+surface_size_x = 500   # Pixels X
+surface_size_y = 500    # Pixels Y
 main_surface = pygame.display.set_mode((surface_size_x,surface_size_y))
 my_clock = pygame.time.Clock()
 
@@ -21,10 +22,15 @@ def gameloop():
                     run = False
 
         # Draw everything
-        #main_surface.fill((30, 0, 30)) #fill background color
+        #main_surface.fill((0, 155, 155)) #fill background color
         # Draw stuff
-        
 
+        x = random.randint(0, surface_size_x-1)
+        y = random.randint(0, surface_size_y-1)
+
+        plot_pt = (x, y)
+        main_surface.set_at(plot_pt, (255,0,0))
+        
         pygame.display.flip() # Put all the drawing up to the display
        # my_clock.tick(120) #Keeps a contant framerate for smoother animation
         # If you don't include the above line, the loop executes as fast as it can
