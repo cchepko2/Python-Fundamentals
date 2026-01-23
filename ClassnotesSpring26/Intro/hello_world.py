843f6e0734adc5ff27ce72ee9bf1f41018ff1a31
Corin Chepko <cchepko2@coloradomesa.edu>
2026-01-23 15:46:42 -0700

    Hello Notes

---
+++ ClassnotesSpring26/Intro/hello_world.py
@@ -0,0 +1,14 @@
+'''
+Corin Chepko
+1/23/26
+Hello World Program
+'''
+
+#----------------------------------------------------------
+# Can use '#' to create a one line comment,
+# or us ''' to enlcose a multi-line comment
+#----------------------------------------------------------
+
+# calling the print function
+# printing a string, "Hello World"
+print('Hello World!') # Say hello to the beautiful world!
