80fd90e26a89a870bb05692c3ef993d0fa6f3041
Corin Chepko <cchepko2@coloradomesa.edu>
2026-02-11 15:51:19 -0700

    asf

--- ClassnotesSpring26/Bitwise_operators_and_number_conversion.py/1.py
+++ ClassnotesSpring26/Bitwise_operators_and_number_conversion.py/1.py
@@ -16,3 +16,4 @@ print(2**3)
 print(2^3)
 print(3^3)
 print(4^2)
+
---
+++ ClassnotesSpring26/User_defined_functions/functions.py
@@ -0,0 +1,7 @@
+#return output = function_name(parameter(s)) 
+data           = input("Message")
+
+# Fruitful function is like input(): it returns data to the program
+
+# Fruitless function like print, does not return data
+print("Hello")
