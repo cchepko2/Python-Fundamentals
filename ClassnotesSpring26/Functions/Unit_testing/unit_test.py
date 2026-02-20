6b6e8d07b2116e5d58f8d1d71b68fcdc729d9ad2
Corin Chepko <cchepko2@coloradomesa.edu>
2026-02-20 15:50:56 -0700

    sg

--- ClassnotesSpring26/Functions/Unit_testing/unit_test.py
+++ ClassnotesSpring26/Functions/Unit_testing/unit_test.py
@@ -1,4 +1,5 @@
 import math
+import sys
 
 GRAVITY = 1.62 # Mun gravity
 
@@ -10,10 +11,25 @@ def main():
     velocity = input("Enter a velocity in m/s: ")
     velocity = float(velocity)
 
-    distance = velocity**2 * math.sin(2*angle)/GRAVITY
+    distance = projectile_calc(angle, velocity)
     
     print("The ball went", distance, "meters.", "Equals", distance*3.28, "feet." )
 
+
+#def projectile_calc(angle: float, velocity: float) -> float:
+def projectile_calc(angle, velocity):
+    distance = velocity**2 * math.sin(2*angle)/GRAVITY
+    return distance
+
+def test():
+    error = 10e-6
+    assert(projectile_calc(0,0) == 0)
+    print(projectile_calc(math.radians(90),100))
+    assert(math.fabs(projectile_calc(math.radians(90),100)-0.0) <= error)
+    print("All test passed!", file=sys.stderr)
+
+
 # If I am the main program, and not an import, run my main function
 if( __name__ == "__main__"):
+    test()
     main()
