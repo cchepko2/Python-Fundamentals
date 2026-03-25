d8123fbd4427794f7fdc523838a994f486dc693f
Corin Chepko <cchepko2@coloradomesa.edu>
2026-03-25 15:26:14 -0600

    piglatin

--- ClassnotesSpring26/Kattis/piglatin/piglatin.py
+++ ClassnotesSpring26/Kattis/piglatin/piglatin.py
@@ -1,20 +1,52 @@
+'''
+Corin Chepko
+3/25/26
+Katiis Problem: piglatin - https://open.kattis.com/problems/piglatin
+'''
+
+
 import sys
 
-print("Enter a message: ", file=sys.stderr)
-message = input()
+def main():
 
-translated = ""
-vowels = 'aeiouy'
-for word in message.split():
+    #for message in sys.stdin:
+        #print("Enter a message: ", file=sys.stderr)
+        #message = input()       
+    while(True):
+        try:
+            message = input()
+        except EOFError:
+            break
+        
+        translated = ""
+
+        for word in message.split():
+            new_word = solution(word)
+            translated += new_word + ' '
+
+        print(translated)
+
+def solution(word):
+    vowels = 'aeiouy'
+    
     if word[0] in vowels:
-        translated += word + 'yay' + ' '
+        return word + 'yay'
     else:
         index = 0
         for c in word:
             if c in vowels:
                 new_word = word[index:] + word[0:index] + 'ay'
+                return new_word
+                break
             index+=1
 
-        translated += new_word + ' '
+def test():
+    assert solution('pig') == 'igpay'
+    assert solution('Corin') == 'orinCay'
 
-print(translated)
+    print('All tests passed!', file=sys.stderr)
+
+
+if __name__ == '__main__':
+    test()
+    main()
